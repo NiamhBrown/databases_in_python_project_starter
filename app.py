40a9133bc886e6cfd43bbffee83353d8388aabb0
niamh <niamh321@me.com>
2024-04-18 16:48:54 +0100

    added Application class to app.py

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
-from lib.database_connection import DatabaseConnection
+
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
+from lib.database_connection import DatabaseConnection
+
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
+
+    def run(self):
+        artist_repository = ArtistRepository(self._connection)
+        album_repository = AlbumRepository(self._connection)
+        artists = artist_repository.all()
+        albums = album_repository.all()
+        
+        print("What would you like to do?")
+        print("1 - List all albums")
+        print("2 - List all artists")
+        user_input = input("Enter your choice:")
+        if user_input == "2":
+            print("Here is the list of artists:")
+            for artist in artists:
+                print(f"{artist.id}: {artist.name} ({artist.genre})")
+        elif user_input == "1":
+            print("Here is the list of albums:")
+            for album in albums:
+                print(f"{album.id}: {album.title} ({album.release_year})")
+        else:
+            print("Invalid input :(")
+
+if __name__ == '__main__':
+    app = Application()
+    app.run()
 
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
-
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
-
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
-
-# Retrieve all albums and then list them out
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
-
-for album in albums:
-    print(album)
-
-    
-    # print (
-    #     )repository.find(1)
